2428eff4b80d82658e119a7a22862d07fe84d159
patrickmunnellly <patrick.m.munnelly@gmail.com>
2024-03-18 19:43:16 +0000

    added best time to buy stock question

---
+++ 4_Best Time_to_Buy_and_Sell_Stock.ts
@@ -0,0 +1,17 @@
+function maxProfit(prices: number[]): number {
+    
+    let buy = prices[0];
+    let max_profit = 0;
+   for (let i = 1; i < prices.length; i++) {
+
+       // Checking for lower buy value
+       if (buy > prices[i])
+           buy = prices[i];
+
+       // Checking for higher profit
+       else if (prices[i] - buy > max_profit)
+           max_profit = prices[i] - buy;
+   }
+
+   return max_profit;
+};
